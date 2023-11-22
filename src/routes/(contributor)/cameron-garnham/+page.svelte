08e53a4df1fc452cfc7fd5d76d1a753045068b4c
grmbyrn <graemebyrne_89@hotmail.com>
2023-11-22 23:30:25 +0100

    Add contributors routes

---
+++ src/routes/(contributor)/cameron-garnham/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import ContributorCard from '$lib/components/molecules/ContributorCard.svelte';
+</script>
+
+<ContributorCard
+	name="Cameron Garnham"
+	position="Open Source Supporter"
+	img=""
+	contributeDate="2023"
+	paras={[
+		{
+			para: 'Cameron Garnham is a Supporter of Open Source Software and has many years of experience in information technologies. Mostly self-taught over the years, he has focused on computer software architecture, system security, software engineering principles and processes, and practical cryptography and associated block chain technologies.'
+		},
+		{
+			para: 'Now Cameron is mostly focused on human relations, hiring, and cooperate strategic vision and planning. He lends his focus to Nautilus and therefore the Torrust Project as he finds the technology interesting, and considers the core team his friends.'
+		}
+	]}
+	links={[
+		{
+			email: 'mailto:me@da2ce7.com',
+			github: 'https://github.com/da2ce7',
+			linkedIn: ''
+		}
+	]}
+/>
