d127462b338713d58fcfffb3561e112b8d6aabdc
Graeme Byrne <95353365+grmbyrn@users.noreply.github.com>
2023-12-18 20:11:20 +0000

    Update date.ts
    Rename the argument in the `formatTime` function for clarity. The argument has been changed from `date` to `time` to better reflect its purpose and improve code readability.
    
    This modification aligns with best practices and enhances the function's self-descriptiveness.

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -4,6 +4,6 @@ export function formatDate(date: string): string {
 	return dateFormat(date, 'UTC:dd/mm/yyyy');
 }
 
-export function formatTime(date: string): string {
-	return dateFormat(date, 'UTC:HH:MM:ss');
+export function formatTime(time: string): string {
+	return dateFormat(time, 'UTC:HH:MM:ss');
 }
