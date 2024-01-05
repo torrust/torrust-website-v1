7fb39fed0d98bcd260c8ad4936a632ac08ee6f59
MacBook air <macbookair@MacBook-Air-de-MacBook.local>
2024-01-05 16:51:46 +0000

    copy code button for all code blocks

--- src/lib/components/molecules/CodeBlock.svelte
+++ src/lib/components/molecules/CodeBlock.svelte
@@ -1,17 +1,44 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte'
 	export let filename: string;
 	export let lang: string;
 	export let fullBleed: boolean | undefined = undefined;
+	let codeBlockElement: HTMLDivElement;
+	let showCheckmark: boolean = false;
+
+	async function copyToClipboard() {
+        try {
+            const codeContent = codeBlockElement.textContent || '';
+            await navigator.clipboard.writeText(codeContent);
+			showCheckmark = true
+
+			setTimeout(() => showCheckmark = false, 1000);
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    }
 </script>
 
-<div class="code-block" class:full-bleed={fullBleed}>
+<div class="code-block" class:full-bleed={fullBleed} bind:this={codeBlockElement}>
 	{#if filename}
 		<div class="filename">{filename}</div>
 	{/if}
 	{#if lang}
 		<div class="lang">{lang}</div>
 	{/if}
-	<slot />
+	<button
+		class={`copy-button ${showCheckmark ? 'copy-button-green' : 'copy-button-gray'}`}
+		on:click={copyToClipboard}
+	>
+		{#if showCheckmark}
+			<Icon icon='charm:tick' color='#6cdb2e' />
+		{:else}
+			<Icon icon='ion:copy-outline' color='#FFFFFF' />
+		{/if}
+	</button>
+	<div class="code-content">
+	    <slot />
+	</div>
 </div>
 
 <style lang="scss">
@@ -67,6 +94,32 @@
 			left: 0px;
 			top: -15px;
 			z-index: 1;
+		}
+
+		.copy-button {
+			position: absolute;
+			top: 1.5rem;
+			right: 1.5rem;
+			padding: 0.25rem;
+			border-radius: 0.375rem;
+			box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+			cursor: pointer;
+		}
+
+		.copy-button-green {
+			background-color: #047857;
+		}
+
+		.copy-button-green:hover {
+			background-color: #065f67;
+		}
+
+		.copy-button-gray {
+			background-color: #4a5568;
+		}
+
+		.copy-button-gray:hover {
+			background-color: #2d3748;
 		}
 	}
 </style>
