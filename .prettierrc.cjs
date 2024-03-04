ed080386864d5a64d85d0c1131884eb070ca926e
MacBook air <macbookair@MacBook-Air-de-MacBook.local>
2024-03-04 19:52:20 +0000

    fix ESLint bugs

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	useTabs: true,
 	singleQuote: true,
-	trailingComma: "none",
+	trailingComma: 'none',
 	printWidth: 100,
 	plugins: [require('prettier-plugin-svelte')],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
 };
