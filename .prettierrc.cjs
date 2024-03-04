17bf1b843caf05c37ed7ad72c54462952d0505a9
Cameron Garnham <me@da2ce7.com>
2024-03-04 09:16:46 +0800

    dev: upgrade to new version of yarn

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: "none",
+	printWidth: 100,
+	plugins: [require('prettier-plugin-svelte')],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+};
