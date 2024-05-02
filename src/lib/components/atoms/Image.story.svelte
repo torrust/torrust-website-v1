fd19235029b5114d5e454dc8d5703d2215e748f1
Jose Celano <josecelano@gmail.com>
2024-05-02 13:43:43 +0100

    fix: remove unused code

--- src/lib/components/atoms/Image.story.svelte
+++ src/lib/components/atoms/Image.story.svelte
@@ -8,35 +8,6 @@
 	let src = 'https://placedog.net/500';
 	let alt = 'A cute dog';
 	let fullBleed: boolean | undefined = undefined;
-
-	let formats: string[] = ['avif', 'webp', 'png'];
-	let widths: string[] | undefined = undefined;
-
-	function buildSrcset() {
-		let srcset = '';
-		const fileName = src.split('.')[0];
-		const fileExtension = src.split('.').pop() || '';
-
-		if (widths) {
-			for (let i = 0; i < widths.length; i++) {
-				srcset += `${fileName}-${widths[i]}.${fileExtension} ${widths[i]}w`;
-
-				if (i < widths.length - 1) {
-					srcset += ', ';
-				}
-			}
-		} else {
-			for (let i = 0; i < formats.length; i++) {
-				srcset += `${fileName}.${formats[i]}`;
-
-				if (i < formats.length - 1) {
-					srcset += ', ';
-				}
-			}
-		}
-
-		return srcset;
-	}
 </script>
 
 <svelte:component this={Hst.Story} title="Atoms/Image">
