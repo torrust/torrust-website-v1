2401c27a92a3572f122be1427018b2e6ecdc8271
Graeme Byrne <grmbrn89@gmail.com>
2024-07-15 19:54:35 +0200

    add about, community, self-host; components and functionality for table of contents

---
+++ src/routes/v2/self-host/+page.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import PostContainer from '$lib/v2/components/molecules/PostContainer.svelte';
+	import PostTable from '$lib/v2/components/molecules/PostTable.svelte';
+	import PostBody from '$lib/v2/components/molecules/PostBody.svelte';
+</script>
+
+<div class="container">
+	<PostContainer>
+		<PostTable>
+			<div>
+				<ul>
+					<li>
+						<a href="#torrustSolution">Torrent solution (Index + Tracker)</a>
+					</li>
+					<ul>
+						<li>
+							<a href="#buildSources">Build from sources (Rust)</a>
+						</li>
+						<li>
+							<a href="#docker">Docker</a>
+						</li>
+						<li>
+							<a href="#tutorials">Tutorials</a>
+						</li>
+					</ul>
+					<li>
+						<a href="#torrustTracker">Torrust tracker</a>
+					</li>
+				</ul>
+			</div>
+		</PostTable>
+
+		<PostBody>
+			<div>
+				<h2 id="torrustSolution">Torrent solution (Index + Tracker)</h2>
+				<p>
+					The Torrust Solution is a complete installation of bot a BitTorrent Index which is
+					associated to one Torrust Tracker. Downloaded torrents will include the the associated
+					tracker in the list of trackers.
+				</p>
+				<p>
+					In our tutorial we explain how to install both in the same server using a single docker
+					compose configiration, but if you need more resources you could install the tracker and
+					the index in different servers. You just need to spits the docker compose configuration
+					and change the tracker configiration in the index to connect to the external tracker.
+				</p>
+
+				<h3 id="buildSources">Build from sources (Rust)</h3>
+				<p>
+					Commodo ullamcorper blandit massa odio mauris odio ornare. Adipiscing imperdiet neque
+					convallis nisl quisque quisque. Fringilla bibendum sit lectus tellus ut urna condimentum
+					tristique. Nisi lectus malesuada consectetur morbi id fringilla et. Blandit mus senectus a
+					elit nisl.
+				</p>
+				<p>
+					Mauris ut dolor dolor nam. Dolor viverra tincidunt egestas ac vulputate. Phasellus sed
+					massa facilisis rhoncus in. Ipsum fermentum est diam justo nibh quis. Libero rutrum quam
+					donec tellus at et.
+				</p>
+				<p>
+					Sed quis enim amet tempor aliquet eget. Phasellus sem vel tincidunt pellentesque non
+					hendrerit cras magna. Volutpat convallis aliquet non lectus quam enim. Nunc ut augue urna
+					vitae venenatis. Pellentesque congue blandit facilisi lectus ac libero.
+				</p>
+
+				<h3 id="docker">Docker</h3>
+				<p>
+					Interdum augue mattis tortor ornare in. Feugiat phasellus integer massa mauris. Leo sit
+					tempus leo eu tellus feugiat malesuada purus. Metus erat eleifend ante sed sem gravida
+					nunc lobortis et. Arcu elit faucibus eget egestas aliquet vivamus consectetur.
+				</p>
+				<p>
+					Laoreet id nisl pharetra sed ut massa dis interdum. Eget eu lacus fringilla sem dignissim
+					cras ridiculus. Potenti cras diam nunc ultricies. Ut metus amet faucibus quam nibh mattis
+					at. Cras in rhoncus ipsum ut id sit id.
+				</p>
+
+				<h3 id="tutorials">Tutorials</h3>
+				<p>
+					Nisi interdum iaculis cursus tellus tincidunt pharetra turpis arcu eget. Nibh in
+					pellentesque risus vestibulum. Dolor interdum dictum ac feugiat nulla gravida scelerisque
+					tellus. Sit diam vestibulum eu quis. Curabitur lectus porta nisi convallis.
+				</p>
+			</div>
+		</PostBody>
+	</PostContainer>
+</div>
--- src/routes/v2/tracker/+page.svelte
+++ src/routes/v2/tracker/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import CardContainer from '$lib/v2/components/molecules/CardContainer.svelte';
 	import Table from '$lib/v2/components/atoms/Table.svelte';
+	import PostContainer from '$lib/v2/components/molecules/PostContainer.svelte';
+	import PostTable from '$lib/v2/components/molecules/PostTable.svelte';
+	import PostBody from '$lib/v2/components/molecules/PostBody.svelte';
 
 	const titleArr1 = [
 		'High quality and modern Rust codebase',
@@ -51,66 +54,87 @@
 		</p>
 		<Table />
 	</div>
-	<div class="article">
-		<div>
-			<ul>
-				<li>Installation</li>
+
+	<PostContainer>
+		<PostTable>
+			<div>
 				<ul>
-					<li>Software requirements</li>
-					<li>Build from sources (Rust)</li>
-					<li>Docker</li>
+					<li>
+						<a href="#installation">Installation</a>
+					</li>
+					<ul>
+						<li>
+							<a href="#softwareRequirements">Software requirements</a>
+						</li>
+						<li>
+							<a href="#buildSources">Build from sources (Rust)</a>
+						</li>
+						<li>
+							<a href="#docker">Docker</a>
+						</li>
+					</ul>
+					<li>
+						<a href="#licenses">Licenses</a>
+					</li>
+					<li>
+						<a href="#roadmap">Roadmap</a>
+					</li>
 				</ul>
-				<li>Licenses</li>
-				<li>Roadmap</li>
-			</ul>
-		</div>
-		<div>
-			<h2>Installation</h2>
-			<p>
-				Massa sit tempor massa blandit suscipit. Vestibulum in faucibus suspendisse enim at sit.
-				Velit eget at cursus vel adipiscing. Tristique feugiat aliquam risus etiam. Auctor
-				scelerisque dignissim facilisi faucibus.
-			</p>
-			<h2>Software requirements</h2>
-			<ul>
-				<li>SQLite 3 or MySQL 8.0.</li>
-				<li>Rust Stable 1.68 if you compile from sources.</li>
-				<li>
-					A running Torrust Tracker. You can check the Tracker page for installation instructions.
-				</li>
-			</ul>
-			<p>See https://docs.rs/torrust-index/latest/torrust_index/#prerequisites for details.</p>
-			<p>You can install the Index from sources or using docker.</p>
-			<h2>Build from sources (Rust)</h2>
-			<p>
-				Commodo ullamcorper blandit massa odio mauris odio ornare. Adipiscing imperdiet neque
-				convallis nisl quisque quisque. Fringilla bibendum sit lectus tellus ut urna condimentum
-				tristique. Nisi lectus malesuada consectetur morbi id fringilla et. Blandit mus senectus a
-				elit nisl.
-			</p>
-			<p>
-				Mauris ut dolor dolor nam. Dolor viverra tincidunt egestas ac vulputate. Phasellus sed massa
-				facilisis rhoncus in. Ipsum fermentum est diam justo nibh quis. Libero rutrum quam donec
-				tellus at et.
-			</p>
-			<p>
-				Sed quis enim amet tempor aliquet eget. Phasellus sem vel tincidunt pellentesque non
-				hendrerit cras magna. Volutpat convallis aliquet non lectus quam enim. Nunc ut augue urna
-				vitae venenatis. Pellentesque congue blandit facilisi lectus ac libero.
-			</p>
-			<h2>Docker</h2>
-			<p>
-				Interdum augue mattis tortor ornare in. Feugiat phasellus integer massa mauris. Leo sit
-				tempus leo eu tellus feugiat malesuada purus. Metus erat eleifend ante sed sem gravida nunc
-				lobortis et. Arcu elit faucibus eget egestas aliquet vivamus consectetur.
-			</p>
-			<p>
-				Laoreet id nisl pharetra sed ut massa dis interdum. Eget eu lacus fringilla sem dignissim
-				cras ridiculus. Potenti cras diam nunc ultricies. Ut metus amet faucibus quam nibh mattis
-				at. Cras in rhoncus ipsum ut id sit id.
-			</p>
-		</div>
-	</div>
+			</div>
+		</PostTable>
+
+		<PostBody>
+			<div>
+				<h2 id="installation">Installation</h2>
+				<p>
+					Massa sit tempor massa blandit suscipit. Vestibulum in faucibus suspendisse enim at sit.
+					Velit eget at cursus vel adipiscing. Tristique feugiat aliquam risus etiam. Auctor
+					scelerisque dignissim facilisi faucibus.
+				</p>
+
+				<h2 id="softwareRequirements">Software requirements</h2>
+				<ul>
+					<li>SQLite 3 or MySQL 8.0.</li>
+					<li>Rust Stable 1.68 if you compile from sources.</li>
+					<li>
+						A running Torrust Tracker. You can check the Tracker page for installation instructions.
+					</li>
+				</ul>
+				<p>See https://docs.rs/torrust-index/latest/torrust_index/#prerequisites for details.</p>
+				<p>You can install the Index from sources or using docker.</p>
+
+				<h2 id="buildSources">Build from sources (Rust)</h2>
+				<p>
+					Commodo ullamcorper blandit massa odio mauris odio ornare. Adipiscing imperdiet neque
+					convallis nisl quisque quisque. Fringilla bibendum sit lectus tellus ut urna condimentum
+					tristique. Nisi lectus malesuada consectetur morbi id fringilla et. Blandit mus senectus a
+					elit nisl.
+				</p>
+				<p>
+					Mauris ut dolor dolor nam. Dolor viverra tincidunt egestas ac vulputate. Phasellus sed
+					massa facilisis rhoncus in. Ipsum fermentum est diam justo nibh quis. Libero rutrum quam
+					donec tellus at et.
+				</p>
+				<p>
+					Sed quis enim amet tempor aliquet eget. Phasellus sem vel tincidunt pellentesque non
+					hendrerit cras magna. Volutpat convallis aliquet non lectus quam enim. Nunc ut augue urna
+					vitae venenatis. Pellentesque congue blandit facilisi lectus ac libero.
+				</p>
+
+				<h2 id="docker">Docker</h2>
+				<p>
+					Interdum augue mattis tortor ornare in. Feugiat phasellus integer massa mauris. Leo sit
+					tempus leo eu tellus feugiat malesuada purus. Metus erat eleifend ante sed sem gravida
+					nunc lobortis et. Arcu elit faucibus eget egestas aliquet vivamus consectetur.
+				</p>
+				<p>
+					Laoreet id nisl pharetra sed ut massa dis interdum. Eget eu lacus fringilla sem dignissim
+					cras ridiculus. Potenti cras diam nunc ultricies. Ut metus amet faucibus quam nibh mattis
+					at. Cras in rhoncus ipsum ut id sit id.
+				</p>
+			</div>
+		</PostBody>
+	</PostContainer>
 </div>
 
 <style lang="scss">
@@ -123,17 +147,6 @@
 
 		p {
 			margin-top: 1.4rem;
-		}
-	}
-
-	.article {
-		margin-top: 4rem;
-	}
-
-	@include for-tablet-landscape-up {
-		.article {
-			display: flex;
-			gap: 1.5rem;
 		}
 	}
 </style>
